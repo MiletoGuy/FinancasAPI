b96d4249ac86b7ba33de4751534c1df7f3970425
Tales Moreira <tales.moreira@ECO.LAN>
2025-07-22 17:25:48 -0300

    teste debugg startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,14 @@
-gunicorn --bind=0.0.0.0:$PORT --timeout 600 app:app
+#!/bin/bash
+
+echo "== Iniciando startup ==" >> /home/LogFiles/startup.log
+cd /home/site/wwwroot
+
+# Ativar ambiente virtual se necessário
+if [ -d antenv ]; then
+    echo "== Ativando venv ==" >> /home/LogFiles/startup.log
+    source antenv/bin/activate
+fi
+
+# Rodar a aplicação com Gunicorn
+echo "== Iniciando Gunicorn ==" >> /home/LogFiles/startup.log
+gunicorn --bind=0.0.0.0:8000 app:app >> /home/LogFiles/startup.log 2>&1
